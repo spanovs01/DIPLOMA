e85dd66ea56958cdca022ba2fdd874bb632523b0
spanovs01 <spanovs01frag@gmail.com>
2023-04-02 18:01:50 +0300

    yolov8 inference IR cpp

---
+++ yolov8/detectwebcam_YOLOv8_IR_cpp/detect_YOLOv8_IR.cpp
@@ -0,0 +1,233 @@
+#include "openvino/openvino.hpp"
+#include <opencv2/opencv.hpp>
+#include <ngraph/type/element_type.hpp>
+#include <iterator>
+#include <memory>
+#include <sstream>
+#include <string>
+#include <vector>
+
+
+struct Detection
+{
+    int class_id{0};
+    std::string className{};
+    float confidence{0.0};
+    cv::Scalar color{};
+    cv::Rect box{};
+};
+
+int main()
+{
+    // auto xml = "/home/ss21mipt/Documents/starkit/DIPLOMA/YOEO/config/IR&onnx_for_416_Petr_1/yoeo.xml";
+    // auto xml = "/home/ss21mipt/Documents/starkit/DIPLOMA/to_rhoban/weights/Feds_yolov8_2_openvino/best.xml";
+    auto xml = "/home/ss21mipt/Documents/starkit/DIPLOMA/to_rhoban/weights/opset_14/Feds_yolov8_2_openvino_model/Feds_yolov8_2.xml";
+    ov::Core core;
+
+    std::shared_ptr<ov::Model> net = core.read_model(xml);    // net = ie.ReadNetwork(model_path);
+    ov::preprocess::PrePostProcessor ppp(net);        
+    ov::preprocess::InputInfo& input = ppp.input(0);            // inputs = net.getInputsInfo(); 
+    auto input_shape = net->input(0).get_partial_shape();
+    auto output1_shape = net->output(0).get_partial_shape();
+    std::cout << net->input(0).get_partial_shape() << output1_shape << std::endl;
+
+    input.tensor().set_element_type(ov::element::f32);   // NAPISAL PODRYGOMY (NE NHWC) почему здесь другая нумерация, как это может быть правильно написанным вариантом?
+    input.model().set_layout("NHWC");
+    
+    net = ppp.build();
+    
+    ov::CompiledModel compiled_model = core.compile_model(net, "CPU");    // auto executable_network = ie.LoadNetwork(net, device);
+    ov::InferRequest infer_request = compiled_model.create_infer_request();    // infer_request = executable_network.CreateInferRequest();
+ 
+    ov::Tensor m_inputData = infer_request.get_input_tensor(0);    // m_inputData = infer_request.GetBlob(inputsName);
+    
+    std::cout << "Shape of input tensor: " << m_inputData.get_shape() << std::endl; 
+    std::cout << "Type of input tensor: " << m_inputData.get_element_type() << std::endl;    
+    auto m_numChannels = m_inputData.get_shape()[1];
+    auto m_inputW = m_inputData.get_shape()[3];
+    auto m_inputH = m_inputData.get_shape()[2];
+    auto m_imageSize = m_inputH * m_inputW;
+    auto data1 = m_inputData.data<float_t>();
+    std::cout << "m_numChannels: " << m_numChannels <<  std::endl <<"m_inputW: " << m_inputW <<  std::endl << "m_inputH: " << m_inputH <<  std::endl<< "m_imageSize: " << m_imageSize << std::endl;
+
+    cv::VideoCapture cap(0);
+    cv::Mat image;
+    cv::Mat segments;
+    cv::Mat mask;
+    cv::Mat test_mask;
+    std::cout << "doshlo" << std::endl;
+    bool yolov8;
+    cv::Size modelShape(640,640);
+    auto modelScoreThreshold = 0.35;
+    auto modelNMSThreshold = 0.5;
+    int number_classes = 1;
+    auto name_classes = "g";
+
+    while (cap.isOpened()){
+        cap >> image;
+        std::cout << "doshlo" << std::endl;
+        if (image.empty() || !image.data) {
+            return false;
+        }
+        cv::Size scale(640, 640);  
+        cv::resize(image, image, scale);    
+        
+        std::cout << "SIZES of Mat: "  << image.size[0] << " " << image.size[1] << " " << image.channels()<<  std::endl;
+  
+        // FILLING THE DATA1
+        
+        for (size_t row = 0; row < m_inputH; row++) {
+            for (size_t col = 0; col < m_inputW; col++) {
+                for (size_t ch = 0; ch < m_numChannels; ch++) {
+
+                    data1[m_imageSize * ch + row * m_inputW + col] = float(image.at<cv::Vec3b>(row, col)[ch] / 255.0);
+
+                }
+            }
+        }
+
+        infer_request.infer();
+
+        ov::Tensor output_tensor1 = infer_request.get_output_tensor(0);
+        ov::Tensor output_tensor2 = infer_request.get_output_tensor(1);
+        
+
+        auto out_data1 = output_tensor1.data<float_t>();
+        auto box_shape1 = output_tensor1.get_shape();
+        auto box_type = output_tensor1.get_element_type();
+
+        auto out_data2 = output_tensor2.data<float_t>();
+        auto mask_shape2 = output_tensor2.get_shape();
+
+        
+        std::cout << "BOX SHAPE" << box_shape1 << std::endl;
+        std::cout << "SEGMENTATION MASK" << mask_shape2 << std::endl;
+        // std::cout << "SEGMENTATION MASK" << *out_data2 << std::endl;
+        
+        auto rows = box_shape1[1];
+        auto dimensions = box_shape1[2];
+        std::cout << "rows and dimensions and type " << rows << " " << dimensions << " " << box_type << std::endl;
+
+        cv::Size s(dimensions,rows);
+        cv::Mat output_boxes(s, CV_32FC1);
+
+        std::cout << "RESULTS are Here: " << " " <<output_boxes.channels() << output_boxes.size[0] << std::endl;
+
+        auto maxim_data = 0;
+        for(int row = 0; row < rows; row++) {
+            for(int dimension = 0; dimension < dimensions; dimension++) {
+        
+                output_boxes.at<float>(row,dimension) = out_data1[dimensions*row + dimension];
+        
+            }
+        }
+        std::cout << "maxim_data: " << maxim_data << std::endl;
+        std::cout << "RESULTS are Here: " << " " <<output_boxes.size[0] << " " << output_boxes.size[1] << " " << output_boxes.size[3] <<std::endl;
+        
+
+        if (dimensions > rows) // Check if the shape[2] is more than shape[1] (yolov8)
+        {
+            yolov8 = true;
+            rows = box_shape1[2];
+            dimensions = box_shape1[1];
+
+            output_boxes = output_boxes.reshape(1, dimensions);
+            cv::transpose(output_boxes, output_boxes);
+        }
+        std::cout << "SUMMARY are Here: " << " " <<output_boxes.size[0] << " " << output_boxes.size[1] << image.cols << image.rows << std::endl;
+        
+        float *data = (float *)output_boxes.data;
+
+        float x_factor = image.cols / modelShape.width;
+        float y_factor = image.rows / modelShape.height;
+
+        std::vector<int> class_ids;
+        std::vector<float> confidences;
+        std::vector<cv::Rect> boxes;
+
+        for (int i = 0; i < rows; ++i)
+        {
+            if (yolov8)
+            {
+                float *classes_scores = data+4;
+
+                cv::Mat scores(1, number_classes, CV_32FC1, classes_scores);
+                cv::Point class_id;
+                double maxClassScore;
+
+                minMaxLoc(scores, 0, &maxClassScore, 0, &class_id);
+
+                if (maxClassScore > modelScoreThreshold)
+                {
+                    confidences.push_back(maxClassScore);
+                    class_ids.push_back(class_id.x);
+
+                    float x = data[0];
+                    float y = data[1];
+                    float w = data[2];
+                    float h = data[3];
+                    std::cout << "[x,y,w,h]: [" << x << ", " << y << ", " << w << ", " << h << "] confidences: " << *confidences.rbegin() <<std::endl;
+
+                    int left = int((x - 0.5 * w) * x_factor);
+                    int top = int((y - 0.5 * h) * y_factor);
+
+                    int width = int(w * x_factor);
+                    int height = int(h * y_factor);
+
+                    boxes.push_back(cv::Rect(left, top, width, height));
+                }
+            }
+
+            data += dimensions;
+        }
+
+        std::vector<int> nms_result;
+        cv::dnn::NMSBoxes(boxes, confidences, modelScoreThreshold, modelNMSThreshold, nms_result);
+
+        std::vector<Detection> detections{};
+        for (unsigned long i = 0; i < nms_result.size(); ++i)
+        {
+            int idx = nms_result[i];
+
+            Detection result;
+            result.class_id = class_ids[idx];
+            result.confidence = confidences[idx];
+
+            result.color = cv::Scalar(255, 0, 0);
+
+            result.className = name_classes;
+            result.box = boxes[idx];
+
+            detections.push_back(result);
+        }
+
+        int detections_num = detections.size();
+        std::cout << "Number of detections:" << detections_num << std::endl;
+
+        for (int i = 0; i < detections_num; ++i)
+        {
+            Detection detection = detections[i];
+
+            cv::Rect box = detection.box;
+            cv::Scalar color = detection.color;
+
+            // Detection box
+            cv::rectangle(image, box, color, 2);
+
+            // Detection box text
+            std::string classString = detection.className + ' ' + std::to_string(detection.confidence).substr(0, 4);
+            cv::Size textSize = cv::getTextSize(classString, cv::FONT_HERSHEY_DUPLEX, 1, 2, 0);
+            cv::Rect textBox(box.x, box.y - 40, textSize.width + 10, textSize.height + 20);
+
+            cv::rectangle(image, textBox, color, cv::FILLED);
+            cv::putText(image, classString, cv::Point(box.x + 5, box.y - 10), cv::FONT_HERSHEY_DUPLEX, 1, cv::Scalar(0, 0, 0), 2, 0);
+        }
+        // Inference ends here...
+
+
+        cv::imshow("webcam", image);
+        if(cv::waitKey(30)>=0)
+            break;
+    }
+    cv::destroyAllWindows();
+}
---
+++ yolov8/inference_yolov8.py
@@ -0,0 +1,184 @@
+
+from openvino.runtime import Core
+import torchvision.transforms as transforms
+import cv2
+import torchvision
+import numpy as np
+from ultralytics.yolo.utils.plotting import colors
+import random
+from ultralytics.yolo.utils import ops
+import torch
+
+print(torch.__version__)
+print(cv2.__version__)
+print(torchvision.__version__)
+
+def plot_one_box(box:np.ndarray, img:np.ndarray, color = (255,0,0), mask:np.ndarray = None, label:str = None, line_thickness:int = 5):
+    """
+    Helper function for drawing single bounding box on image
+    Parameters:
+        x (np.ndarray): bounding box coordinates in format [x1, y1, x2, y2]
+        img (no.ndarray): input image
+        color (Tuple[int, int, int], *optional*, None): color in BGR format for drawing box, if not specified will be selected randomly
+        mask (np.ndarray, *optional*, None): instance segmentation mask polygon in format [N, 2], where N - number of points in contour, if not provided, only box will be drawn
+        label (str, *optonal*, None): box label string, if not provided will not be provided as drowing result
+        line_thickness (int, *optional*, 5): thickness for box drawing lines
+    """
+    # Plots one bounding box on image img
+    tl = line_thickness or round(0.002 * (img.shape[0] + img.shape[1]) / 2) + 1  # line/font thickness
+    color = color or [random.randint(0, 255) for _ in range(3)]
+    c1, c2 = (int(box[0]), int(box[1])), (int(box[2]), int(box[3]))
+    cv2.rectangle(img, c1, c2, color, thickness=tl, lineType=cv2.LINE_AA)
+    if label:
+        tf = max(tl - 1, 1)  # font thickness
+        t_size = cv2.getTextSize(label, 0, fontScale=tl / 3, thickness=tf)[0]
+        c2 = c1[0] + t_size[0], c1[1] - t_size[1] - 3
+        cv2.rectangle(img, c1, c2, color, -1, cv2.LINE_AA)  # filled
+        cv2.putText(img, label, (c1[0], c1[1] - 2), 0, tl / 3, [225, 255, 255], thickness=tf, lineType=cv2.LINE_AA)
+    if mask is not None:
+        image_with_mask = img.copy()
+        mask
+        cv2.fillPoly(image_with_mask, pts=[mask.astype(int)], color=color)
+        img = cv2.addWeighted(img, 0.5, image_with_mask, 0.5, 1)
+    return img
+
+def draw_results(results, source_image:np.ndarray, label_map):
+    """
+    Helper function for drawing bounding boxes on image
+    Parameters:
+        image_res (np.ndarray): detection predictions in format [x1, y1, x2, y2, score, label_id]
+        source_image (np.ndarray): input image for drawing
+        label_map; (Dict[int, str]): label_id to class name mapping
+    Returns:
+
+    """
+    results = results[0]
+    boxes = results["det"]
+    masks = results.get("segment")
+    h, w = source_image.shape[:2]
+    for idx, (*xyxy, conf, lbl) in enumerate(boxes):
+        label = f'{label_map[int(lbl)]} {conf:.2f}'
+        mask = masks[idx] if masks is not None else None
+        source_image = plot_one_box(xyxy, source_image, mask=mask, label=label, color=(255, 157, 151), line_thickness=1)
+        print()
+    return source_image
+
+
+def postprocess(
+    pred_boxes:np.ndarray,
+    input_hw,
+    orig_img:np.ndarray,
+    min_conf_threshold:float = 0.25,
+    nms_iou_threshold:float = 0.7,
+    agnosting_nms:bool = False,
+    max_detections:int = 300,
+    pred_masks:np.ndarray = None,
+    retina_mask:bool = False
+):
+    """
+    YOLOv8 model postprocessing function. Applied non maximum supression algorithm to detections and rescale boxes to original image size
+    Parameters:
+        pred_boxes (np.ndarray): model output prediction boxes
+        input_hw (np.ndarray): preprocessed image
+        orig_image (np.ndarray): image before preprocessing
+        min_conf_threshold (float, *optional*, 0.25): minimal accepted confidence for object filtering
+        nms_iou_threshold (float, *optional*, 0.45): minimal overlap score for removing objects duplicates in NMS
+        agnostic_nms (bool, *optiona*, False): apply class agnostinc NMS approach or not
+        max_detections (int, *optional*, 300):  maximum detections after NMS
+        pred_masks (np.ndarray, *optional*, None): model ooutput prediction masks, if not provided only boxes will be postprocessed
+        retina_mask (bool, *optional*, False): retina mask postprocessing instead of native decoding
+    Returns:
+       pred (List[Dict[str, np.ndarray]]): list of dictionary with det - detected boxes in format [x1, y1, x2, y2, score, label] and segment - segmentation polygons for each element in batch
+    """
+    nms_kwargs = {"agnostic": agnosting_nms, "max_det":max_detections}
+    # if pred_masks is not None:
+    #     nms_kwargs["nm"] = 32
+    preds = ops.non_max_suppression(
+        torch.from_numpy(pred_boxes),
+        min_conf_threshold,
+        nms_iou_threshold,
+        nc=1,
+        **nms_kwargs
+    )
+    results = []
+    proto = torch.from_numpy(pred_masks) if pred_masks is not None else None
+
+    for i, pred in enumerate(preds):
+        shape = orig_img[i].shape if isinstance(orig_img, list) else orig_img.shape
+        if not len(pred):
+            results.append({"det": [], "segment": []})
+            continue
+        if proto is None:
+            pred[:, :4] = ops.scale_boxes(input_hw, pred[:, :4], shape).round()
+            results.append({"det": pred})
+            continue
+        if retina_mask:
+            pred[:, :4] = ops.scale_boxes(input_hw, pred[:, :4], shape).round()
+            masks = ops.process_mask_native(proto[i], pred[:, 6:], pred[:, :4], shape[:2])  # HWC
+            segments = [ops.scale_segments(input_hw, x, shape, normalize=False) for x in ops.masks2segments(masks)]
+        else:
+            masks = ops.process_mask(proto[i], pred[:, 6:], pred[:, :4], input_hw, upsample=True)
+            pred[:, :4] = ops.scale_boxes(input_hw, pred[:, :4], shape).round()
+            segments = [ops.scale_segments(input_hw, x, shape, normalize=False) for x in ops.masks2segments(masks)]
+        results.append({"det": pred[:, :6].numpy(), "segment": segments})
+    return results
+
+def inference(model_xml_path, im_path):
+
+    label_map = {0: 'red', 1 : 'green'}
+    print(colors(1))
+    ie = Core()
+    # model_xml_path = "/home/ss21mipt/Documents/starkit/DIPLOMA/to_rhoban/weights/Feds_yolov8_2_openvino/best.xml"
+    # im_path = "/home/ss21mipt/Documents/starkit/field.jpg"
+    model = ie.read_model(model_xml_path)
+    device = "CPU"
+    compiled_model = ie.compile_model(model=model, device_name=device)
+    print("INPUT TENSOR ", compiled_model.input().shape, compiled_model.output(0).shape, compiled_model.output(1).shape, type(compiled_model))
+    cam = cv2.VideoCapture(0)
+    key = cv2.waitKey(1)
+
+    while key != 27:
+        _, image = cam.read()
+        cv2.imshow("source", image)
+        # image = cv2.imread(im_path)
+        image = cv2.resize(image, (640,640), interpolation=cv2.INTER_LINEAR)
+        im = np.expand_dims(image, axis=0)
+        im_for_draw = image
+        im = np.transpose(im, (0,3,1,2))
+        input_tensor = im.astype(np.float32)  # uint8 to fp32
+        input_tensor /= 255.0  # 0 - 255 to 0.0 - 1.0
+
+        print(f"image\t{type(im), im.shape, np.info(im)}")
+        print(f"im\t{type(im), im.shape, np.info(im)}")
+        print(f"input_tensor\t{type(input_tensor), input_tensor.shape, np.info(input_tensor)}")
+
+        result = compiled_model([input_tensor])
+        output_det = compiled_model.output(0)
+        output_seg = compiled_model.output(1)
+        boxes = compiled_model([input_tensor])[output_det]
+        masks = compiled_model([input_tensor])[output_seg]  
+        # results = postprocess(boxes, (640,640), image, pred_masks=masks)
+        # proto = torch.from_numpy(masks) if masks is not None else None
+        detections = postprocess(pred_boxes=boxes, input_hw=(640,640), orig_img=image, pred_masks=masks)
+        res_img = draw_results(detections, im_for_draw, label_map)
+        # cv2.imshow("source" ,image)
+        cv2.imshow("res" , res_img)
+        if cv2.waitKey(1) == 27:
+            cam.release()
+            cv2.destroyAllWindows()
+            break
+if __name__ == '__main__':
+    # xml = "/home/ss21mipt/Documents/starkit/DIPLOMA/to_rhoban/weights/Feds_yolov8_2_openvino/best.xml"
+    xml = "/home/ss21mipt/Documents/starkit/DIPLOMA/to_rhoban/weights/opset_14/Feds_yolov8_2_openvino_model/Feds_yolov8_2.xml"
+    png = "/home/ss21mipt/Documents/starkit/field.jpg"
+    inference(xml, png)
+# print()
+# input_image = transforms.Compose([
+#         DEFAULT_TRANSFORMS,
+#         Resize(network_image_height)])((
+#             image,
+#             np.empty((1, 5)),
+#             np.empty((network_image_height, network_image_height), dtype=np.uint8)))[0].unsqueeze(0)
+
+# res = model("/home/ss21mipt/Documents/starkit/field", retina_masks=True)
+# print(type(res))
